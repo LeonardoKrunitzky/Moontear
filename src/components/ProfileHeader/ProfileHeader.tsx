1feedae1cfa8523d88b37d2931488bd3379ca49e
Leonardo Krunitzky <110630098+LeonardoKrunitzky@users.noreply.github.com>
2026-08-03 13:51:12 -0300

    Add welcome message and quote to ProfileHeader

--- src/components/ProfileHeader/ProfileHeader.tsx
+++ src/components/ProfileHeader/ProfileHeader.tsx
@@ -4,6 +4,8 @@ import styles from './ProfileHeader.module.css';
 export default function ProfileHeader() {
   return (
     <header className={styles.profileHeader}>
+      <h2 className={styles.welcomeNoir}>SEJA BEM-VINDO AO MEU MUNDO</h2>
+
       <div className={styles.profilePic}>
         <Image 
           src="/images/pfp.jpg" 
@@ -13,7 +15,9 @@ export default function ProfileHeader() {
           unoptimized
         />
       </div>
+      
       <h1 className={styles.glitchName} data-text="𝓛𝓮𝓸𝓷𝓪𝓻𝓭𝓸">𝓛𝓮𝓸𝓷𝓪𝓻𝓭𝓸</h1>
+      
       <p className={styles.quote}>&quot;We can only move forward, even in the darkest time&quot;</p>
     </header>
   );
